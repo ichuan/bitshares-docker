f8a0f53c875bce92dc6726b522f09f7d3c53487c
yc <iyanchuan@gmail.com>
2019-05-07 16:51:39 +0800

    fix: for v3.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,14 @@ mkdir -p /opt/coin/p2p
 if test $# -eq 0; then
   # start node
   track=${track_account:-1.2.12345}
-  /opt/programs/witness_node/witness_node --data-dir /opt/coin --track-account "\"$track\"" &
+  /opt/witness_node --data-dir /opt/coin --track-account "\"$track\"" &
   echo "Logging to logs/default/default.log ..."
   # wait until ws started
   while ! nc -z -w 1 127.0.0.1 8090; do
     sleep .2
   done
   # wallet
-  exec /opt/programs/cli_wallet/cli_wallet --server-rpc-endpoint ws://127.0.0.1:8090 --rpc-http-endpoint 0.0.0.0:8091 --wallet-file /opt/coin/wallet.json
+  exec /opt/cli_wallet --server-rpc-endpoint ws://127.0.0.1:8090 --rpc-http-endpoint 0.0.0.0:8091 --wallet-file /opt/coin/wallet.json
 else
   exec $@
 fi
